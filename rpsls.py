947c626e3889eb936edab1212b75077f1559744c
Paul Kuo <code.9555608@nctu.edu.tw>
2014-09-24 23:28:25 +0800

    Finished Rock-paper-scissors-lizard-Spock.

--- rpsls.py
+++ rpsls.py
@@ -1,4 +1,5 @@
 ﻿# Rock-paper-scissors-lizard-Spock
+import random
 
 # The key idea of this program is to equate the strings
 # "rock", "paper", "scissors", "lizard", "Spock" to numbers
@@ -9,44 +10,59 @@
 # 2 - paper
 # 3 - lizard
 # 4 - scissors
+names = [
+    "rock",
+    "Spock",
+    "paper",
+    "lizard",
+    "scissors"
+]
 
 # helper functions
 
 def name_to_number(name):
-    # delete the following pass statement and fill in your code below
-    pass
-
-    # convert name to number using if/elif/else
-    # don't forget to return the result!
-
+    for i in range(len(names)):
+        if names[i] == name:
+            return i
+    return None
 
 def number_to_name(number):
-    # delete the following pass statement and fill in your code below
-    pass
-    
-    # convert number to a name using if/elif/else
-    # don't forget to return the result!
-    
+    if number >= 0 and number < len(names):
+        return names[number]
+    else:
+        return None
 
-def rpsls(player_choice): 
-    # delete the following pass statement and fill in your code below
-    pass
-    
+def rpsls(player_choice):
     # print a blank line to separate consecutive games
-
-    # print out the message for the player's choice
+    print
 
     # convert the player's choice to player_number using the function name_to_number()
-
+    player_number = name_to_number(player_choice)
+    if player_number == None:
+        print "Unknown choice"
+    else:
+        # print out the message for the player's choice
+        print "Player chooses " + player_choice
+    
     # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(len(names))
 
     # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
     
     # print out the message for computer's choice
+    print "Computer chooses " + comp_choice
 
     # compute difference of comp_number and player_number modulo five
+    result = (player_number - comp_number) % len(names)
 
-    # use if/elif/else to determine winner, print winner message
+    # determine winner, print winner message
+    if result == 0:
+        print "Player and computer tie!"
+    elif result <= 2:
+        print "Player wins!"
+    else:
+        print "Computer wins!"
 
 if __name__ == "__main__":
     rpsls("rock")
--- rpsls_test.py
+++ rpsls_test.py
@@ -6,11 +6,11 @@ class RpslsTestCase(unittest.TestCase):
 
     # Test calls to name_to_number()
     def test_number_to_name(self):
-        self.assertEquals(rpsls.number_to_name(0), 'rock');
-        self.assertEquals(rpsls.number_to_name(1), 'Spock');
-        self.assertEquals(rpsls.number_to_name(2), 'paper');
-        self.assertEquals(rpsls.number_to_name(3), 'lizard');
-        self.assertEquals(rpsls.number_to_name(4), 'scissors');
+        self.assertEquals(rpsls.number_to_name(0), "rock");
+        self.assertEquals(rpsls.number_to_name(1), "Spock");
+        self.assertEquals(rpsls.number_to_name(2), "paper");
+        self.assertEquals(rpsls.number_to_name(3), "lizard");
+        self.assertEquals(rpsls.number_to_name(4), "scissors");
 
     # Test calls to name_to_number()
     def test_name_to_number(self):
